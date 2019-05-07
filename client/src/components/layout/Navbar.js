ad8ad308cb64e4e29cdb21b56e6524b3a1a17dac
Rose McClain <rosemcclain024@gmail.com>
2019-05-07 11:53:49 -0400

    fixed app.js, built components folder, everything running on localhost

---
+++ client/src/components/layout/Navbar.js
@@ -0,0 +1,32 @@
+import React, { Component } from "react";
+import { Link } from "react-router-dom";
+// import Materialize, { Component } from 'react-materialize';
+
+// import { BrowserRouter, Switch, Route } from 'react-router-dom';
+class Navbar extends Component {
+  render() {
+    return (
+    // <BrowserRouter>
+    //  <Switch>
+      <div className="navbar-fixed">
+        <nav className="z-depth-0">
+          <div className="nav-wrapper white">
+            <Link
+              to="/"
+              style={{
+                fontFamily: "monospace"
+              }}
+              className="col s5 brand-logo center black-text"
+            >
+              <i className="material-icons">code</i>
+              What is going on tbh
+            </Link>
+          </div>
+        </nav>
+      </div>
+    //  </Switch>
+    // </BrowserRouter> 
+    );
+  }
+}
+export default Navbar;
