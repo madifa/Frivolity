b0cb26476025ba2e924755ca7bad4efc51a28160
Rose McClain <rosemcclain024@gmail.com>
2019-05-10 01:54:16 -0400

    looks pretty

--- client/src/components/layout/Landing.js
+++ client/src/components/layout/Landing.js
@@ -7,14 +7,11 @@ class Landing extends Component {
       <div style={{ height: "75vh" }} className="container valign-wrapper">
         <div className="row">
           <div className="col s12 center-align">
-            <h4>
-              <b>Build</b> a login/auth app with the{" "}
-              <span style={{ fontFamily: "monospace" }}>MERN</span> stack from
-              scratch
-            </h4>
-            <p className="flow-text grey-text text-darken-1">
-              Create a (minimal) full-stack app with user authentication via
-              passport and JWTs
+            <h1 id="frivolity" class="animated fadeInUp slow delay-1s">
+               frivolity
+            </h1>
+            <p className="flow-text grey-text text-darken-1" class="animated fadeIn slow delay-3s">
+              let's feel a little lighter | it can only go up from here
             </p>
             <br />
             <Link to="/register"
--- client/src/components/layout/Navbar.js
+++ client/src/components/layout/Navbar.js
@@ -1,35 +1,51 @@
-import React, { Component } from "react";
-import { Link } from "react-router-dom";
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 
 class Navbar extends Component {
-  render() {
-    return (
-      <div className="navbar-fixed">
-        <nav className="z-depth-0">
-          <div className="nav-wrapper yellow">
-          <a href="#!" class="brand-logo"><i class="material-icons">cloud</i>Logo</a>
-          <ul class="right hide-on-med-and-down">
-    <li><a href="sass.html"><i class="material-icons">search</i></a></li>
-    <li><a href="badges.html"><i class="material-icons">view_module</i></a></li>
-    <li><a href="collapsible.html"><i class="material-icons">refresh</i></a></li>
-    <li><a href="mobile.html"><i class="material-icons">more_vert</i></a></li>
-  </ul>
-            <Link
-              to="/"
-              style={{
-                fontFamily: "monospace"
-              }}
-              className="col s5 brand-logo center black-text"
-            >
-              <i className="material-icons">code</i>
-              FRIVOLITY
-            </Link>
-          </div>
-        </nav>
-        
-      </div>
-    );
-  }
+	render() {
+		return (
+			<div className="navbar-fixed">
+				<nav id="nav" className="z-depth-0">
+					<div className="nav-wrapper grey">
+						<a href="#!" class="brand-logo" >
+							<i class="material-icons">cloud</i>frivolity
+						</a>
+						<ul class="right hide-on-med-and-down">
+							<li>
+								<a href="sass.html">
+									<i class="material-icons">search</i>
+								</a>
+							</li>
+							<li>
+								<a href="badges.html">
+									<i class="material-icons">view_module</i>
+								</a>
+							</li>
+							<li>
+								<a href="collapsible.html">
+									<i class="material-icons">refresh</i>
+								</a>
+							</li>
+							<li>
+								<a href="mobile.html">
+									<i class="material-icons">more_vert</i>
+								</a>
+							</li>
+						</ul>
+						<Link
+							to="/"
+							style={{
+								fontFamily: 'monospace'
+							}}
+							className="col s5 brand-logo center black-text"
+						>
+							<i className="material-icons" />
+						</Link>
+					</div>
+				</nav>
+			</div>
+		);
+	}
 }
 
 export default Navbar;
